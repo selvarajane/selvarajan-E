0ae26c3f576ecc3de06c6bf5ab8dd576f47ff38e
selvarajane <selvarajan0259@gmail.com>
2026-05-12 01:16:55 +0530

    Fixed Hero.tsx PresentationControls config for Vercel

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -78,7 +78,6 @@ const Hero = () => {
             
             <PresentationControls
               global
-              config={{ mass: 2, tension: 500 }}
               snap
               rotation={[0, 0.3, 0]}
               polar={[-Math.PI / 3, Math.PI / 3]}
